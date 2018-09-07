9951ab445617a3f2ca61ba41aba42865d099e3da
nil <nil.mamano@gmail.com>
2018-09-07 16:46:54 -0700

    sanim lines no longer overlap

--- sanim.py
+++ sanim.py
@@ -12,9 +12,21 @@ class Sanim(Scene):
         input_lines = open(source_file).read().splitlines()
         raw_sobjects = get_raw_sobjects(input_lines)
         sobjects = get_sobjects(raw_sobjects)
+        arrange_sobjects(sobjects)
+        sobject_list = []
         for line in sobjects:
-            for sobject in line:
-                self.play(Write(sobject.vline))
+            if type(line) is list:
+                sobject_list += line
+            else:
+                sobject_list.append(line)
+        i = 0
+        while i < len(sobject_list):
+            sobjs_to_play = [sobject_list[i]]
+            while not sobject_list[i].wait_for_input:
+                i += 1
+                sobjs_to_play.append(sobject_list[i])
+            i += 1
+            self.play(*[Write(sobject) for sobject in sobjs_to_play])
 
 
 def get_raw_sobjects(input_lines):
@@ -34,50 +46,78 @@ def get_raw_sobjects(input_lines):
             if in_horizontal_group:
                 res[-1].append(line)
             else:
-                res.append([line])
+                res.append(line)
     return res
 
 def get_sobjects(raw_sobjects):
     res = []
     for line in raw_sobjects:
-        res.append([])
-        num_sobjects = len(line)
-        if num_sobjects == 1:
-            max_width = SCREEN_WIDTH
+        if type(line) is not list:
+            if line[0] == '>':
+                res.append(SObject(line[1:], SCREEN_WIDTH, False))
+            else:
+                res.append(SObject(line, SCREEN_WIDTH, True))
         else:
+            res.append([])
+            num_sobjects = len(line)
             max_width = (SCREEN_WIDTH-HORIZONTAL_SEPARATOR*(num_sobjects+1))/num_sobjects
-        for raw_sobject in line:
-            wait_for_input = True
-            if raw_sobject[0] == '>':
-                wait_for_input = False
-                raw_sobject = raw_sobject[1:]
-            res[-1].append(SObject(raw_sobject, max_width, wait_for_input))
+            for raw_sobject in line:
+                if raw_sobject[0] == '>':
+                    res[-1].append(SObject(raw_sobject[1:], max_width, False))
+                else:
+                    res[-1].append(SObject(raw_sobject, max_width, True))
     return res
 
-class SObject():
+def arrange_sobjects(sobjects):
+    prev_obj = None
+    for line in sobjects:
+        if type(line) is not list:
+            if prev_obj is None:
+                line.to_edge(UP)
+            else:
+                line.next_to(prev_obj.get_corner(DOWN+LEFT), DOWN)
+                line.to_edge(LEFT)
+            prev_obj = line
+        else:
+            if prev_obj is None:
+                sys.exit('bug')
+            is_first = True
+            for sobj in line:
+                if is_first:
+                    sobj.next_to(prev_obj.get_corner(DOWN+LEFT), DOWN)
+                    sobj.to_edge(LEFT)
+                    is_first = False
+                else:
+                    sobj.next_to(prev_obj.get_edge_center(RIGHT),RIGHT)
+
+                prev_obj = sobj
+
+
+class SObject(VGroup):
 
     def __init__(self, text, max_width, wait_for_input):
         self.text = text
         self.max_width = max_width
-        self.type = text.split()[0]
-        self.content = text[len(self.type)+1:]
+        self.SOtype = text.split()[0]
+        self.content = text[len(self.SOtype)+1:]
         self.wait_for_input = wait_for_input
         self.time_stamp = None #this will be updated later
 
-        if self.type == 'TITLE':
+        if self.SOtype == 'TITLE':
             self.vline = makeTitle(self.content, max_width)
-        elif self.type == 'DEF':
+        elif self.SOtype == 'DEF':
             self.vline = makeDef(self.content, max_width)
-        elif self.type == '-':
+        elif self.SOtype == '-':
             self.vline = makeBullet(self.content, max_width)
-        elif self.type == '*':
+        elif self.SOtype == '*':
             self.vline = makePlaneLine(self.content, max_width)
-        elif self.type == 'SCENE':
+        elif self.SOtype == 'SCENE':
             self.vline = makeScene(self.content, max_width)
-        elif self.type == 'IMAGE':
+        elif self.SOtype == 'IMAGE':
             self.vline = makeImage(self.content, max_width)
         else:
             sys.exit('unexpected type')
+        super().__init__(self.vline)
 
 def makeTitle(content, width):
     return TextMobject(content);
