bd6e98c1778bdc7ddf9eb8a757d035a086a61971
nil <nil.mamano@gmail.com>
2018-09-09 08:35:04 -0700

    implemented flush

--- sanim.py
+++ sanim.py
@@ -43,6 +43,12 @@ class InputLine:
     def is_content_line(self):
         return self.output_content_elems != []
 
+    def get_fade_out_actions(self):
+        res = []
+        for elem in self.output_content_elems:
+            res += elem.get_fade_out_actions()
+        return res
+
 class InputElem:
     def __init__(self, raw_content):
         if ';' in raw_content:
@@ -50,16 +56,9 @@ class InputElem:
 
         #keep the raw content just in case
         self.raw_content = raw_content[:]
-        self.modifiers = []
-
-        # if raw_content == '':
-        #     self.content = ''
-        #     self.keyword = 'PLAIN'
-        #     self.keyword_type = 'content'
-        #     self.modifiers.append('>')
-        #     return
 
         #parse modifiers
+        self.modifiers = []
         i = 0
         while i < len(raw_content) and (raw_content[i] == ' ' or raw_content[i] in MODIFIER_SYMBOLS):
             if raw_content[i] in MODIFIER_SYMBOLS:
@@ -99,13 +98,25 @@ class OutputElem:
     def individual_play_duration(self):
         pass
 
-    def position_center_at(self, aux_mobj):
+    def position_center_at(self, pos_mobj):
         pass
 
-    def position_left_aligned(self, aux_mobj):
+    def position_left_aligned(self, pos_mobj):
+        pass
+
+    def get_shift_center_at_actions(self, pos_mobj):
+        pass
+
+    def get_shift_left_aligned_actions(self, pos_mobj):
         pass
 
     def get_bottom_right_mobject(self):
+        pass
+
+    def get_fade_out_actions(self):
+        pass
+
+    def copy(self):
         pass
 
 class TitleElem(OutputElem):
@@ -116,6 +127,10 @@ class TitleElem(OutputElem):
             sys.exit('empty title')
         self.run_time = 1.1+len(content)/200
         self.text = Title(content, scale_factor=1.3, color=BLACK, background_stroke_color=BACKGROUND)
+
+    def copy(self):
+        return TitleElem(self.input_elem)
+
     def individual_play(self, scene):
         scene.play(Write(self.text), run_time=self.run_time)
 
@@ -125,15 +140,28 @@ class TitleElem(OutputElem):
     def individual_play_duration(self):
         return self.run_time
 
-    def position_center_at(self, aux_mobj):
-        self.text.move_to(aux_mobj)
+    def position_center_at(self, pos_mobj):
+        self.text.move_to(pos_mobj)
 
-    def position_left_aligned(self, aux_mobj):
+    def position_left_aligned(self, pos_mobj):
         self.text.center()
         self.text.to_edge(UP)
 
+    def get_shift_center_at_actions(self, pos_mobj):
+        return [ApplyMethod(self.text.move_to, pos_mobj)]
+
+    def get_shift_left_aligned_actions(self, pos_mobj):
+        sys.exit("not implemented")
+        pos_title = TextMobject("aux") #used only for positioning
+        pos_title.center()
+        pos_title.to_edge(UP)
+        return [ApplyMethod(self.text.move_to, pos_title, alignment=LEFT)]
+
     def get_bottom_right_mobject(self):
         return self.text
+
+    def get_fade_out_actions(self):
+        return [FadeOut(self.text)]
 
 class PlainElem(OutputElem):
     def __init__(self, input_elem):
@@ -150,6 +178,9 @@ class PlainElem(OutputElem):
             self.run_time = 0.6+len(content)/150
             self.is_empty = False
 
+    def copy(self):
+        return PlainElem(self.input_elem)
+
     def individual_play(self, scene):
         if self.is_empty:
             return
@@ -163,15 +194,33 @@ class PlainElem(OutputElem):
     def individual_play_duration(self):
         return self.run_time
 
-    def position_center_at(self, aux_mobj):
-        self.text.move_to(aux_mobj)
+    def position_center_at(self, pos_mobj):
+        self.text.move_to(pos_mobj)
 
-    def position_left_aligned(self, aux_mobj):
-        self.text.move_to(aux_mobj)
+    def position_left_aligned(self, pos_mobj):
+        self.text.move_to(pos_mobj)
         self.text.to_edge(LEFT)
+
+    def get_shift_center_at_actions(self, pos_mobj):
+        if self.is_empty:
+            return []
+        return [ApplyMethod(self.text.move_to, pos_mobj)]
+
+    def get_shift_left_aligned_actions(self, pos_mobj):
+        if self.is_empty:
+            return []
+        pos_text = TextMobject("aux") #used only for positioning
+        pos_text.move_to(pos_mobj)
+        pos_text.to_edge(LEFT)
+        return [ApplyMethod(self.text.move_to, pos_text)]
 
     def get_bottom_right_mobject(self):
         return self.text
+
+    def get_fade_out_actions(self):
+        if self.is_empty:
+            return []
+        return [FadeOut(self.text)]
 
 class BulletElem(OutputElem):
     def __init__(self, input_elem):
@@ -182,6 +231,9 @@ class BulletElem(OutputElem):
         self.text = BulletedItem(content, color=BLACK, background_stroke_color=BACKGROUND)
         self.run_time = 0.6+len(content)/150
 
+    def copy(self):
+        return BulletElem(self.input_elem)
+
     def individual_play(self, scene):
         scene.play(Write(self.text), run_time=self.run_time)
 
@@ -191,15 +243,27 @@ class BulletElem(OutputElem):
     def individual_play_duration(self):
         return self.run_time
 
-    def position_center_at(self, aux_mobj):
-        self.text.move_to(aux_mobj)
+    def position_center_at(self, pos_mobj):
+        self.text.move_to(pos_mobj)
 
-    def position_left_aligned(self, aux_mobj):
-        self.text.move_to(aux_mobj)
+    def position_left_aligned(self, pos_mobj):
+        self.text.move_to(pos_mobj)
         self.text.to_edge(LEFT)
+
+    def get_shift_center_at_actions(self, pos_mobj):
+        return [ApplyMethod(self.text.move_to, pos_mobj)]
+
+    def get_shift_left_aligned_actions(self, pos_mobj):
+        pos_text = BulletedItem(self.input_elem.content) #used only for positioning
+        pos_text.move_to(pos_mobj)
+        pos_text.to_edge(LEFT)
+        return [ApplyMethod(self.text.move_to, pos_text)]
 
     def get_bottom_right_mobject(self):
         return self.text
+
+    def get_fade_out_actions(self):
+        return [FadeOut(self.text)]
 
 class DefElem(OutputElem):
     def __init__(self, input_elem):
@@ -229,6 +293,9 @@ class DefElem(OutputElem):
         self.in_between_time = 0.5
         self.defi_run_time = 0.6+len(self.defi_text)/150
 
+    def copy(self):
+        return DefElem(self.input_elem)
+
     def individual_play(self, scene):
         scene.play(Write(self.term), run_time=self.term_run_time)
         scene.wait(self.in_between_time)
@@ -240,21 +307,39 @@ class DefElem(OutputElem):
     def individual_play_duration(self):
         return self.term_run_time + self.in_between_time + self.defi_run_time
 
-    def position_center_at(self, aux_mobj):
-        #group = VGroup(self.term, self.defi)
-        #group.move_to(aux_mobj)
-        self.term.move_to(aux_mobj)
+    def position_center_at(self, pos_mobj):
+        self.term.move_to(pos_mobj)
         self.defi.next_to(self.term, RIGHT)
         self.defi.align_to(self.term, UP)
 
-    def position_left_aligned(self, aux_mobj):
-        self.term.move_to(aux_mobj)
+    def position_left_aligned(self, pos_mobj):
+        self.term.move_to(pos_mobj)
         self.term.to_edge(LEFT)
         self.defi.next_to(self.term, RIGHT)
         self.defi.align_to(self.term, UP)
 
+    def get_shift_center_at_actions(self, pos_mobj):
+        pos_term = TextMobject(term_text, alignment = "") #used for positioning
+        pos_defi = TextMobject(defi_text, alignment = "") #used for positioning
+        pos_term.move_to(pos_mobj)
+        pos_defi.next_to(pos_term, RIGHT)
+        pos_defi.align_to(pos_term, UP)
+        return [ApplyMethod(self.term.move_to, pos_term), ApplyMethod(self.defi.move_to, pos_defi)]
+
+    def get_shift_left_aligned_actions(self, pos_mobj):
+        pos_term = TextMobject(term_text, alignment = "") #used for positioning
+        pos_defi = TextMobject(defi_text, alignment = "") #used for positioning
+        pos_term.move_to(pos_mobj)
+        pos_term.to_edge(LEFT)
+        pos_defi.next_to(pos_term, RIGHT)
+        pos_defi.align_to(pos_term, UP)
+        return [ApplyMethod(self.term.move_to, pos_term), ApplyMethod(self.defi.move_to, pos_defi)]
+
     def get_bottom_right_mobject(self):
         return self.defi
+
+    def get_fade_out_actions(self):
+        return [FadeOut(self.term), FadeOut(self.defi)]
 
 #note that the first line has index 1 (because my editor starts counting at 1...)
 def inputToLines(source_file):
@@ -268,55 +353,117 @@ def inputToLines(source_file):
         i += 1
     return res
 
-def display_animation_buffer(scene, buffer):
-    if len(buffer) == 0:
+def display_animation_buffer(anim_buffer, scene):
+    if len(anim_buffer) == 0:
         return
-    if len(buffer) == 1:
-        buffer[0].individual_play(scene)
-        scene.wait(DEFAULT_WAIT_TIME)
+    if len(anim_buffer) == 1:
+        anim_buffer[0].individual_play(scene)
     else:
         actions = []
-        for elem in buffer:
+        for elem in anim_buffer:
             actions += elem.get_play_actions()
         scene.play(*actions, run_time=DEFAULT_RUNTIME)
-        scene.wait(DEFAULT_WAIT_TIME)
 
-def animate_lines(scene, lines):
-    # active_lines = []
+    anim_buffer.clear()
+    scene.wait(DEFAULT_WAIT_TIME)
+
+def add_elem_to_anim_buffer(elem, anim_buffer, scene):
+    if elem.wait_for_input:
+        display_animation_buffer(anim_buffer, scene)
+    anim_buffer.append(elem)
+
+def get_top_left_pos():
+    res = TextMobject("aux") #used only for its positioninig
+    res.to_corner(TOP+LEFT, buff=MED_SMALL_BUFF)
+    return res
+
+def animate_content_line(line, curr_pos, anim_buffer, scene):
+    elems = line.output_content_elems
+    if len(elems) == 1:
+        elem = elems[0]
+        elem.position_left_aligned(curr_pos)
+        curr_pos.move_to(elem.get_bottom_right_mobject().get_edge_center(DOWN))
+        add_elem_to_anim_buffer(elem, anim_buffer, scene)
+    else:
+        num_elems = len(elems)
+        i = 1
+        for elem in elems:
+            curr_pos.to_edge(LEFT, buff=0)
+            curr_pos.shift(RIGHT*(curr_pos.get_edge_center(LEFT)-curr_pos.get_center()))
+            curr_pos.shift(i*2*FRAME_X_RADIUS*RIGHT/(num_elems+1))
+            elem.position_center_at(curr_pos)
+            i += 1
+            add_elem_to_anim_buffer(elem, anim_buffer, scene)
+        curr_pos.move_to(elems[-1].get_bottom_right_mobject().get_edge_center(DOWN))
+
+    curr_pos.shift(DOWN*0.5)
+
+def get_shift_actions(line, curr_pos):
+    elems = line.output_content_elems
+    res = []
+    if len(elems) == 0:
+        return []
+    if len(elems) == 1:
+        elem = elems[0]
+        #actions = get_shift_left_aligned_actions(curr_pos)
+        res += elem.get_shift_left_aligned_actions(curr_pos)
+        elem_copy = elem.copy()
+        elem_copy.position_left_aligned(curr_pos)
+        curr_pos.move_to(elem_copy.get_bottom_right_mobject().get_edge_center(DOWN))
+    else:
+        num_elems = len(elems)
+        i = 1
+        for elem in elems:
+            curr_pos.to_edge(LEFT, buff=0)
+            curr_pos.shift(RIGHT*(curr_pos.get_edge_center(LEFT)-curr_pos.get_center()))
+            curr_pos.shift(i*2*FRAME_X_RADIUS*RIGHT/(num_elems+1))
+            res += elem.get_shift_center_at_actions(curr_pos)
+            i += 1
+        elem_copy = elems[-1].copy()
+        elem_copy.position_center_at(curr_pos)
+        curr_pos.move_to(elem_copy.get_bottom_right_mobject().get_edge_center(DOWN))
+
+    curr_pos.shift(DOWN*0.5)
+    return res
+
+
+def animate_lines(lines, scene):
+    flush_index = 0 #starting line to flush when using flush
     animation_buffer = []
-    curr_pos = TextMobject("aux") #used only for its positioninig
-    curr_pos.to_corner(TOP+LEFT, buff=MED_SMALL_BUFF)
+    curr_pos = get_top_left_pos()
     for line in lines:
         #scene.play(Write(curr_pos))
         if line.is_content_line():
-            elems = line.output_content_elems
-            if len(elems) == 1:
-                elem = elems[0]
-                elem.position_left_aligned(curr_pos)
-                curr_pos.move_to(elem.get_bottom_right_mobject().get_edge_center(DOWN))
-                if elem.wait_for_input:
-                    display_animation_buffer(scene, animation_buffer)
-                    animation_buffer = [elem]
-                else:
-                    animation_buffer.append(elem)
-            else:
-                num_elems = len(elems)
-                i = 1
-                for elem in elems:
-                    curr_pos.to_edge(LEFT, buff=0)
-                    curr_pos.shift(RIGHT*(curr_pos.get_edge_center(LEFT)-curr_pos.get_center()))
-                    curr_pos.shift(i*2*FRAME_X_RADIUS*RIGHT/(num_elems+1))
-                    elem.position_center_at(curr_pos)
+            animate_content_line(line, curr_pos, animation_buffer, scene)
+        else:
+            elems = line.input_elems
+            if len(elems) == 0:
+                sys.exit("empty line")
+            if len(elems) > 1:
+                sys.exit("more than one command in a line")
+            elem = elems[0]
+            if elem.keyword == 'FLUSH':
+                display_animation_buffer(animation_buffer, scene) #leftover stuff
+                flush_line_num = int(elem.content)
+                curr_line_num = line.line_num
+                if flush_line_num > curr_line_num:
+                    sys.exit("cannot flush beyond the current line")
+                fade_out_actions = []
+                while lines[flush_index].line_num < flush_line_num:
+                    if lines[flush_index].is_content_line():
+                        fade_out_actions += lines[flush_index].get_fade_out_actions()
+                    flush_index += 1
+                curr_pos = get_top_left_pos()
+                shift_actions = []
+                i = flush_index
+                while lines[i].line_num < curr_line_num:
+                    shift_actions += get_shift_actions(lines[i], curr_pos)
                     i += 1
-                    if elem.wait_for_input:
-                        display_animation_buffer(scene, animation_buffer)
-                        animation_buffer = [elem]
-                    else:
-                        animation_buffer.append(elem)
-                curr_pos.move_to(elems[-1].get_bottom_right_mobject().get_edge_center(DOWN))
-
-            curr_pos.shift(DOWN*0.5)
-    display_animation_buffer(scene, animation_buffer)
+                scene.play(*fade_out_actions, *shift_actions, run_time=1.4)
+                scene.wait(0.8)
+            else:
+                sys.exit("unknown command")
+    display_animation_buffer(animation_buffer, scene) #leftover stuff
 
 class Sanim(Scene):
     CONFIG = {
@@ -326,7 +473,7 @@ class Sanim(Scene):
         source_file = "sanim_input2.txt"
         lines = inputToLines(source_file)
         print("finished parsing")
-        animate_lines(self, lines)
+        animate_lines(lines, self)
         # raw_sobjects = get_raw_sobjects(input_lines)
         # sobjects = get_sobjects(raw_sobjects)
         # arrange_sobjects(sobjects)
